fdb887ae9ee142ed4edb847a568033f3f7a49910
khlevon98 <kh.levon98@gmail.com>
2016-10-28 01:28:25 +0400

    cleanup

--- src/app/components/old/ace-angular/controller.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kh.levon98 on 24-Sep-16.
- */
-function AceCtrl($scope) {
-  'ngInject';
-
-}
-
-export default AceCtrl;
--- src/app/components/old/tree-angular/link.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kh.levon98 on 24-Sep-16.
- */
-
-function TreeLink(scope, elm, attrs, ngModel) {
-
-}
-
-export default TreeLink;
