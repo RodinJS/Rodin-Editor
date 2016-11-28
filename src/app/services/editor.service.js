84632693dbd22665e9ef9bfa9b07a9d9218a1fb0
khlevon98 <kh.levon98@gmail.com>
2016-11-28 20:09:53 +0400

    temprory fix

--- src/app/services/editor.service.js
+++ src/app/services/editor.service.js
@@ -16,6 +16,7 @@ class Editor {
 
   getProject(projectId = null, fields = {}) {
     let Analyser = new this._Analyser();
+    fields.getAll = true;
 
     this._Editors.one(projectId).get(fields).then(Analyser.resolve, Analyser.reject);
 
