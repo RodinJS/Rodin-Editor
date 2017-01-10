e255769a1e0be6439b0f6bdf071696409b667d74
khlevon98 <kh.levon98@gmail.com>
2017-01-10 20:34:49 +0400

    fix

--- src/app/components/rodin/projects-list/controller.js
+++ src/app/components/rodin/projects-list/controller.js
@@ -2,7 +2,7 @@
  * Created by kh.levon98 on 24-Sep-16.
  */
 class ProjectsListCtrl {
-  constructor(AppConstants, $scope, $stateParams, Project, $window) {
+  constructor(AppConstants, $scope, $stateParams, Project, $window, User) {
     'ngInject';
     this._AppConstants = AppConstants;
     this.appName = AppConstants.appName;
@@ -11,6 +11,7 @@ class ProjectsListCtrl {
     this._$scope = $scope;
     this._$window = $window;
     this._Project = Project;
+    this._User = User;
 
     this.projectRoot = $stateParams.projectFolder;
 
@@ -25,7 +26,7 @@ class ProjectsListCtrl {
       return false;
     }
 
-    this._$window.location.href = this.EDITOR + root;
+    this._$window.location.href = `${this.EDITOR}${this._User.current.username}/${root}`;
 
     if (this._AppConstants.env === "local") {
       this._$window.location.reload();
